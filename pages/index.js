7267d46f180655da8743cf1220fd224d45338267
FelipeReggiane <102805505+FelipeReggiane@users.noreply.github.com>
2022-12-13 15:34:04 -0300

    clean home

--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,14 @@ export default function Home({ data }) {
   );
 }
 
-// This function gets called at build time
 export async function getStaticProps() {
   try {
-    // Call an external API endpoint to get posts
     const [{ value: data }] = await Promise.allSettled([
       Request({
         method: "GET",
         url: `https://strapi-production-a226.up.railway.app/api/homes?populate=deep`,
       }),
     ]);
-
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
 
     return {
       props: {
